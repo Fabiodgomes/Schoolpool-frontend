0f771c97be70bb71df876530f34c83a4d776d22e
Fabio <95591197>
2022-07-22 14:05:24 +0200

    added_redirects file

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,3 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl =
+  process.env.API_URL || "https://schoolpool2.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 2000;
